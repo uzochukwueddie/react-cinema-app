2494675e8923caca9515896bddb6f0a51627ea9a
Uzochukwu Eddie Odozi <uo@sms-digital.com>
2020-04-13 15:34:08 +0200

    changed app component function to arrow function

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
