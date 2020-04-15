ae9e98d30cc8282b5c59aae106896087290901a6
Uzochukwu Eddie Odozi <uo@sms-digital.com>
2020-04-15 12:47:50 +0200

    fixed prettier error

--- src/components/content/grid/Grid.js
+++ src/components/content/grid/Grid.js
@@ -9,29 +9,23 @@ const Grid = (props) => {
   return (
     <>
       <div className="grid">
-        {
-          images.map((image, i) =>
-            <div key={i}>
-              <div className="grid-cell"
-                style={{ backgroundImage: `url(${image.url})` }}
-              >
-                <div className="grid-read-more">
-                  <button className="grid-cell-button">
-                    Read More
-                  </button>
-                </div>
-                <div className="grid-detail">
-                  <span className="grid-detail-title">Mission Impossible</span>
-                  <div className="grid-detail-rating">
-                    <Rating rating={image.rating} totalStars={10} />
-                    &nbsp;&nbsp;
-                    <div className="grid-vote-average">{image.rating}</div>
-                  </div>
+        {images.map((image, i) => (
+          <div key={i}>
+            <div className="grid-cell" style={{ backgroundImage: `url(${image.url})` }}>
+              <div className="grid-read-more">
+                <button className="grid-cell-button">Read More</button>
+              </div>
+              <div className="grid-detail">
+                <span className="grid-detail-title">Mission Impossible</span>
+                <div className="grid-detail-rating">
+                  <Rating rating={image.rating} totalStars={10} />
+                  &nbsp;&nbsp;
+                  <div className="grid-vote-average">{image.rating}</div>
                 </div>
               </div>
             </div>
-          )
-        }
+          </div>
+        ))}
       </div>
     </>
   );
--- src/components/content/rating/Rating.js
+++ src/components/content/rating/Rating.js
@@ -22,18 +22,20 @@ const Rating = ({ rating, totalStars, className }) => {
   return (
     <div className="star-rating">
       <div className={`back-stars ${className}`}>
-        {numberOfStars && numberOfStars.map((i) => (
-          <Fragment key={i}>
-            <i className="fa fa-star" aria-hidden="true"></i>
-          </Fragment>
-        ))}
-
-        <div className={`front-stars ${className}`} ref={ratingRef}>
-          {numberOfStars && numberOfStars.map((i) => (
+        {numberOfStars &&
+          numberOfStars.map((i) => (
             <Fragment key={i}>
               <i className="fa fa-star" aria-hidden="true"></i>
             </Fragment>
           ))}
+
+        <div className={`front-stars ${className}`} ref={ratingRef}>
+          {numberOfStars &&
+            numberOfStars.map((i) => (
+              <Fragment key={i}>
+                <i className="fa fa-star" aria-hidden="true"></i>
+              </Fragment>
+            ))}
         </div>
       </div>
     </div>
