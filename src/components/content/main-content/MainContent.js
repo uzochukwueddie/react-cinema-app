3eb5c5d489c6a2fd0a81dd26faac1d7ebed5bd5d
Uzochukwu Eddie Odozi <uo@sms-digital.com>
2020-04-15 10:00:31 +0200

    added paginate component and styles

--- src/components/content/main-content/MainContent.js
+++ src/components/content/main-content/MainContent.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './MainContent.scss';
 import Slideshow from '../slide-show/Slideshow';
+import Paginate from '../paginate/Paginate';
 
 const MainContent = () => {
   const images = [
@@ -15,12 +16,24 @@ const MainContent = () => {
       url: 'https://images.pexels.com/photos/255379/pexels-photo-255379.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
     }
   ];
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const paginate = (type) => {
+    if (type === 'prev' && currentPage >= 1) {
+      setCurrentPage((prev) => prev - 1);
+    } else {
+      setCurrentPage((prev) => prev + 1);
+    }
+  };
+
   return (
     <div className="main-content">
       <Slideshow images={images} auto={true} showArrows={true} />
       <div className="grid-movie-title">
         <div className="movieType">Now Playing</div>
-        <div className="paginate">Paginate</div>
+        <div className="paginate">
+          <Paginate currentPage={currentPage} totalPages={10} paginate={paginate} />
+        </div>
       </div>
       {/* display grid component */}
     </div>
