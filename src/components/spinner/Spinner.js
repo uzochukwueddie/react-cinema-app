fb1db7ce6011d0a8ba85119df6656df4a79519f5
Uzochukwu Eddie Odozi <uo@sms-digital.com>
2020-04-17 18:43:00 +0200

    added circleci test job

--- src/components/spinner/Spinner.js
+++ src/components/spinner/Spinner.js
@@ -4,7 +4,7 @@ import './Spinner.scss';
 
 const Spinner = () => {
   return (
-    <div className="spinner">
+    <div className="spinner" data-testid="spinner">
       <div className="bounce1"></div>
       <div className="bounce2"></div>
       <div className="bounce3"></div>
