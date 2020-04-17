92ab872930922e0921719389e5cf6ede63f2bda6
Uzochukwu Eddie Odozi <uo@sms-digital.com>
2020-04-17 16:38:44 +0200

    added details api service methods and actions

--- src/redux/reducers/movieReducer.js
+++ src/redux/reducers/movieReducer.js
@@ -1,4 +1,4 @@
-import { MOVIE_LIST, RESPONSE_PAGE, LOAD_MORE_RESULTS, MOVIE_TYPE, SEARCH_QUERY, SEARCH_RESULT } from '../types';
+import { MOVIE_LIST, RESPONSE_PAGE, LOAD_MORE_RESULTS, MOVIE_TYPE, SEARCH_QUERY, SEARCH_RESULT, MOVIE_DETAILS, CLEAR_MOVIE_DETAILS } from '../types';
 
 const initialState = {
   list: [],
@@ -6,7 +6,8 @@ const initialState = {
   totalPages: 0,
   movieType: 'now_playing',
   searchQuery: '',
-  searchResult: []
+  searchResult: [],
+  movie: []
 };
 
 export default (state = initialState, action) => {
@@ -43,6 +44,16 @@ export default (state = initialState, action) => {
       return {
         ...state,
         searchQuery: action.payload
+      };
+    case MOVIE_DETAILS:
+      return {
+        ...state,
+        movie: action.payload
+      };
+    case CLEAR_MOVIE_DETAILS:
+      return {
+        ...state,
+        movie: []
       };
     default:
       return state;
