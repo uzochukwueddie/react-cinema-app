047e97034ca6b11be8732738e734a8aadf3351c7
Uzochukwu Eddie Odozi <uo@sms-digital.com>
2020-04-19 14:50:00 +0200

    setup-sentry-for-error-monitoring

--- src/redux/types.js
+++ src/redux/types.js
@@ -7,3 +7,5 @@ export const SEARCH_QUERY = 'SEARCH_QUERY';
 export const SEARCH_RESULT = 'SEARCH_RESULT';
 export const MOVIE_DETAILS = 'MOVIE_DETAILS';
 export const CLEAR_MOVIE_DETAILS = 'CLEAR_MOVIE_DETAILS';
+export const APP_ROUTES = 'APP_ROUTES';
+export const PATH_URL = 'PATH_URL';
