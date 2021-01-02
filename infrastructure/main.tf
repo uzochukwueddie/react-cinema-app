4d06cde22f438ee7e3bd466ccad2b667bfc5561c
uzochukwueddie <uzo.odozi@gmail.com>
2021-01-02 23:11:49 +0100

    Create main.tf

---
+++ infrastructure/main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket  = "app-cinema-tf-state"
+    key     = "app-cinema.tfstate"
+    region  = "eu-central-1"
+    encrypt = true
+  }
+}
+
+locals {
+  prefix = "${var.prefix}-${terraform.workspace}"
+  common_tags = {
+    Environment = terraform.workspace
+    Project     = var.project
+    ManageBy    = "Terraform"
+    Owner       = "Uzochukwu Eddie Odozi"
+  }
+}
